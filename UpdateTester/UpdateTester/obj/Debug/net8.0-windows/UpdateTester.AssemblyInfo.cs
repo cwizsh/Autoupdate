b495f63bd32a8f94ec5e26854ef1c6fd3a397c0c
cwizsh <160217049+cwizsh@users.noreply.github.com>
2024-12-10 12:33:50 +0100

    HAALLLOOOO

--- UpdateTester/UpdateTester/obj/Debug/net8.0-windows/UpdateTester.AssemblyInfo.cs
+++ UpdateTester/UpdateTester/obj/Debug/net8.0-windows/UpdateTester.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpdateTester")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beee8a94d500c3b53809c3c90348bf7bc7e52db0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpdateTester")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpdateTester")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
